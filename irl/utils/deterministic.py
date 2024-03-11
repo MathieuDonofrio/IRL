fd1328440dc3cb9bdd20dac8a70e4da63e13df11
mathieu d'onofrio <mathieu.donofrio1@gmail.com>
2024-03-11 15:10:07 -0400

    derministic util

---
+++ irl/utils/deterministic.py
@@ -0,0 +1,12 @@
+import numpy as np
+import torch
+import random
+
+def manual_seed_all(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
